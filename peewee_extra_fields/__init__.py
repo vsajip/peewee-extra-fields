09deb12917b09f1535887f4b1e5c14c332bda9b7
juancarlospaco <juancarlospaco@gmail.com>
2017-12-20 06:29:04 -0300

    Add USA Social Security Numbers.

--- peewee_extra_fields/__init__.py
+++ peewee_extra_fields/__init__.py
@@ -34,7 +34,8 @@ __all__ = ('ARCUITField', 'ARPostalCodeField', 'CSVField', 'CharFieldCustom',
            'LanguageISOCodeField', 'PastDateField', 'PastDateTimeField',
            'PositiveBigIntegerField', 'PositiveDecimalField',
            'PositiveFloatField', 'PositiveIntegerField',
-           'PositiveSmallIntegerField', 'SWIFTISOCodeField', 'USZipCodeField')
+           'PositiveSmallIntegerField', 'SWIFTISOCodeField',
+           'USSocialSecurityNumberField', 'USZipCodeField')
 
 
 ##############################################################################
@@ -811,6 +812,78 @@ class USZipCodeField(CharField):
                 characters long (valid values must match a Regex
                 {zip_code_regex}): {value}.""")
 
+        return value
+
+
+class USSocialSecurityNumberField(FixedCharField):
+    """FixedCharField clone but only accepts USA Social Security Numbers."""
+    max_length = 11
+
+    def db_value(self, value: str) -> str:
+        if isinstance(value, str):
+
+            value = value.strip()
+
+            if len(value) != 11:
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (valid values must be exactly 11 Characters): {value}.""")
+
+            us_social_security_number_regex = re.compile(
+                r"^(?P<area>\d{3})[-\ ]?(?P<group>\d{2})[-\ ]?(?P<sri>\d{4})$")
+            match = re.match(us_social_security_number_regex, value)
+
+            if not match:
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (valid Social Security Number values be a must match a Regex
+                {us_social_security_number_regex}): {value} -> {match}.""")
+
+            area = match.groupdict()['area']
+            group =  match.groupdict()['group']
+            serial = match.groupdict()['sri']
+
+            if area == '000':
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (valid Area must not be all Zeroes): {value} -> {area}.""")
+
+            if group == '00':
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (valid Group must not be all Zeroes): {value} -> {group}.""")
+
+            if serial == '0000':
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (valid Serial must not be all Zeroes): {value} -> {serial}.""")
+
+            if area == '666':
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (Block '666' will never be allocated): {value} -> {area}.""")
+
+            if area.startswith('9'):
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (Block '9**' is in the 'promotional block' 987-65-4320 through
+                987-65-4329, is permanently invalid): {value} -> {area}.""")
+
+            if ((area == '078' and group == '05' and serial == '1120') or
+                (area == '219' and group == '09' and serial == '9999')):
+                raise ValueError(f"""{self.__class__.__name__} Value is not a
+                valid U.S.A. Social Security Number string (XXX-XX-XXXX format)
+                (promo & permanently invalid numbers): {value} -> {area}.""")
+
+            return f"{area}-{group}-{serial}"
+
+        return value
+
+    def python_value(self, value: str) -> namedtuple:
+        if value and isinstance(value, str):
+            return namedtuple(
+                "USSocialSecurityNumber", "ssn area group serial")(
+                    value, int(value[:3]), int(value[4:6]), int(value[7:]))
         return value
 
 
