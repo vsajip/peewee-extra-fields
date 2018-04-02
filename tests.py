2391cf98f323b56d5c5ba7a4fd2509b08863ee0f
juancarlospaco <juancarlospaco@gmail.com>
2018-04-02 00:35:29 -0300

    Try to run Postgres on Travis CI.

--- tests.py
+++ tests.py
@@ -32,7 +32,7 @@ class TestFields(unittest.TestCase):
     maxDiff, __slots__ = None, ()
 
     def tearDown(self):
-        db.drop_tables(db.get_tables(), fail_silently=True)
+        db.drop_tables(db.get_tables() or [], fail_silently=True)
 
     def test_PositiveIntegerField(self):
         valid_values = (0, 1, 2, 3, 9, 100, 1_000, 2_147_483_647)
